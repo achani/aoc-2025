09c068befa49049cdf81ae6db854c4feb46c8776
Ajay Chani <ajay.chani@gmail.com>
2025-12-14 19:22:37 +0100

    Solution for part2

--- src/2/main.py
+++ src/2/main.py
@@ -17,23 +17,47 @@ def find_max(num):
 
 
 with open("input.txt","r") as f:
-  id_ranges = [(int(pair[0]),int(pair[1])) for pair in [rng.split('-') for rng in f.read().split(',')]]
+  id_ranges = [(pair[0],pair[1]) for pair in [rng.split('-') for rng in f.read().split(',')]]
 
-part1 = 0 
 
-for rng in id_ranges:
-  start, end = rng
-  if end < 10: continue
-  min = start if start < 10 else find_min(start)
-  max =  find_max(end)
-  if max < min: 
-    min, max = max, min
-  for n in range(min, max + 1):
-    invalid_id = n * 10**len(str(n)) + n
-    if start <= invalid_id <= end:
-      part1 += invalid_id
+def sum_repeating_ids_in_range(start,end):
+  ans = 0 
+  for num in range(int(start), int(end)+1):
+    id = str(num)
+    l = len(id)
+    for i in range(2,l+1):
+      if l % i == 0 and id[0:l//i]*i == id:
+          ans += int(id)
+          break
+  return ans
 
-print(part1)
+
+def solve_part1():
+  ans = 0
+  for rng in id_ranges:
+    start, end = map(int, rng)
+    if end < 10: continue
+    min = start if start < 10 else find_min(start)
+    max =  find_max(end)
+    if max < min: 
+      min, max = max, min
+    for n in range(min, max + 1):
+      invalid_id = n * 10**len(str(n)) + n
+      if start <= invalid_id <= end:
+        ans += invalid_id
+  return ans
+
+
+def solve_part2():
+  ans = 0
+  for rng in id_ranges:
+    start, end = rng
+    ans += sum_repeating_ids_in_range(start,end)
+  return ans
+
+
+print(solve_part1())
+print(solve_part2())
 
 
 
